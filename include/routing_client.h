3669a019504a36158e1699ebbc7ff3b6f445c8f3
Berkus <berkus@gmail.com>
2013-10-14 12:42:38 +0300

    Add routing_client public interface signals.

--- include/routing_client.h
+++ include/routing_client.h
@@ -1,10 +1,70 @@
 #pragma once
 
+#include <boost/signals2/signal.hpp>
+
 namespace uia {
 namespace routing {
 
+class client_profile;
+
+/**
+ * Routing client represents the client side of the routing libraries.
+ * It provides notifications about found peer endpoints and allows to initiate peer searches.
+ */
 class client
-{};
+{
+    class client_impl;
+    std::unique_ptr<client_impl> pimpl_;
+
+public:
+    // Get the metadata about this client.
+    std::shared_ptr<client_profile> profile() const;
+    // Set the metadata about this client.
+    void set_profile(std::shared_ptr<client_profile> profile);
+
+    // Request information about a specific ID.
+    // Will send an on_lookup_done() signal when the request completes.
+    // If 'notify', ask whoever found the ID to notify the target as well.
+    void lookup(ssu::peer_id const& id, bool notify = false);
+
+    // Search for IDs of clients with metadata matching a search string.
+    // Will send an on_search_done() signal when the request completes.
+    void search(std::string const& text);
+
+    typedef boost::signals2::signal<void (ssu::peer_id const& /* target peer */,
+        ssu::endpoint const& /* endpoint found for this peer */,
+        client_profile const& /* peer's profile data */)>
+        lookup_signal;
+    typedef boost::signals2::signal<void (ssu::peer_id const& /* target peer */)>
+        lookup_fail_signal;
+
+    /**
+     * Emitted when lookup request returns some results.
+     */
+    lookup_signal on_lookup_done;
+    /**
+     * Emitted when lookup request fails without returning results or routing client disconnects.
+     */
+    lookup_fail_signal on_lookup_failed;
+    /**
+     * Emitted when somebody else's lookup request found our metadata.
+     */
+    lookup_signal on_lookup_notify;
+
+    typedef boost::signals2::signal<void (std::string const& /*search term*/,
+        std::vector<ssu::peer_id> const& /* peers matching this term */, 
+        bool /*last result received*/)>
+        search_signal;
+
+    /**
+     * Emitted when search() found some records.
+     */
+    search_signal on_search_done;
+
+    inline void search_failed(std::string const& term) {
+        on_search_done(term, std::vector<ssu::peer_id>(), true);
+    }
+};
 
 } // routing namespace
 } // uia namespace
