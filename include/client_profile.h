81e17bb2751ad49b259d9e4fc49ab89cdac09c10
Berkus <berkus@gmail.com>
2013-10-15 17:57:40 +0300

    Add client_profile for regserver profile storage.

---
+++ include/client_profile.h
@@ -0,0 +1,143 @@
+#pragma once
+
+#include <boost/range/adaptor/map.hpp>
+#include <boost/range/algorithm/copy.hpp>
+#include "byte_array.h"
+#include "byte_array_wrap.h"
+#include "link.h"
+#include "underlying.h"
+
+namespace uia {
+namespace routing {
+
+// Client profile represents a client-specified block of information
+// about itself to be made publicly available to other clients
+// through a registration server.
+class client_profile
+{
+public:
+    // Attribute tags - will grow over time.
+    // The upper 16 bits are for attribute property flags.
+    enum class attribute_tag : uint32_t
+    {
+        invalid     = 0x00000000,   ///< Invalid attribute
+
+        // Flags indicating useful properties of certain tags
+        searchable  = 0x00010000,   ///< Search-worthy text (UTF-8)
+
+        // Specific binary tags useful for rendezvous
+        endpoints   = 0x00000001,   ///< Private addresses for hole punch
+
+        // UTF-8 string tags representing advertised information
+        // this is fairly preliminary at the moment.
+        hostname    = 0x00010001,   ///< Name of host (machine)
+        ownername   = 0x00010002,   ///< Name of owner (human)
+        city        = 0x00010003,   ///< Metropolitan area
+        region      = 0x00010004,   ///< State or other locality
+        country     = 0x00010005,   ///< Political state
+    };
+
+    // Constructors
+    inline client_profile() {}
+    inline client_profile(client_profile const& other) : attributes_(other.attributes_) {}
+    inline client_profile(byte_array const& data) { deflurry(data); }
+
+    /** @name Basic attribute management methods. */
+    /**@{*/
+    inline bool is_empty() const { return attributes_.empty(); }
+    inline std::vector<std::underlying_type<attribute_tag>::type>
+    tags() const {
+        std::vector<std::underlying_type<attribute_tag>::type> keys;
+        boost::copy(attributes_ | boost::adaptors::map_keys, std::back_inserter(keys));
+        return keys;
+    }
+
+    inline byte_array attribute(attribute_tag tag) const {
+        return attributes_.at(to_underlying(tag));
+    }
+    inline void set_attribute(attribute_tag tag, byte_array const& value) {
+        attributes_[to_underlying(tag)] = value;
+    }
+    inline void remove(attribute_tag tag) {
+        set_attribute(tag, byte_array());
+    }
+
+    /**@}*/
+
+    /** @name String attribute get/set. */
+    /**@{*/
+    inline std::string string(attribute_tag tag) const {
+        byte_array data = attribute(tag);
+        return std::string(data.begin(), data.end());
+    }
+    inline void set_string(attribute_tag tag, std::string const& value) {
+        set_attribute(tag, byte_array::wrap(value.c_str(), value.size()));
+    }
+    /**@}*/
+
+    /** @name Type-specific methods for individual attributes. */
+    /**@{*/
+    inline std::string host_name()  const { return string(attribute_tag::hostname); }
+    inline std::string owner_name() const { return string(attribute_tag::ownername); }
+    inline std::string city()       const { return string(attribute_tag::city); }
+    inline std::string region()     const { return string(attribute_tag::region); }
+    inline std::string country()    const { return string(attribute_tag::country); }
+
+    inline void set_host_name(std::string const& str) {
+        set_string(attribute_tag::hostname, str);
+    }
+    inline void setownername(std::string const& str) {
+        set_string(attribute_tag::ownername, str);
+    }
+    inline void setcity(std::string const& str) {
+        set_string(attribute_tag::city, str);
+    }
+    inline void setregion(std::string const& str) {
+        set_string(attribute_tag::region, str);
+    }
+    inline void setcountry(std::string const& str) {
+        set_string(attribute_tag::country, str);
+    }
+    /**@}*/
+
+    /**
+     * Return all the words appearing in all searchable string attributes.
+     */
+    std::vector<std::string> keywords() const;
+
+    /** @name Advertised private endpoints. */
+    /**@{*/
+    std::vector<ssu::endpoint> endpoints() const;
+    void set_endpoints(std::vector<ssu::endpoint> const& endpoints);
+    /**@}*/
+
+    inline void enflurry(flurry::oarchive& oa) const {
+        oa << attributes_;
+    }
+    inline void deflurry(flurry::iarchive& ia) {
+        attributes_.clear();
+        ia >> attributes_;
+    }
+    inline void deflurry(byte_array const& data) {
+        attributes_.clear();
+        byte_array_iwrap<flurry::iarchive> read(data);
+        read.archive() >> attributes_;
+    }    
+private:
+    std::unordered_map<std::underlying_type<attribute_tag>::type, byte_array> attributes_;
+};
+
+inline flurry::oarchive& operator << (flurry::oarchive& oa, client_profile const& cp)
+{
+    cp.enflurry(oa);
+    return oa;
+}
+
+inline flurry::iarchive& operator >> (flurry::iarchive& ia, client_profile& cp)
+{
+    cp.deflurry(ia);
+    return ia;
+}
+
+} // routing namespace
+} // uia namespace
---
+++ lib/client_profile.cpp
@@ -0,0 +1,58 @@
+#include <regex>
+#include "client_profile.h"
+
+namespace uia {
+namespace routing {
+
+std::vector<std::string>
+client_profile::keywords() const
+{
+    std::vector<std::string> result;
+    std::regex word_regex("(\\S+)");
+
+    for (auto tag : tags())
+    {
+        if (tag & to_underlying(attribute_tag::searchable))
+        {
+            std::string s = string(attribute_tag(tag));
+            // Break this string into keywords and add it to our list
+            auto words_begin = std::sregex_iterator(s.begin(), s.end(), word_regex);
+            auto words_end = std::sregex_iterator();
+
+            const int N = 2;
+            for (std::sregex_iterator i = words_begin; i != words_end; ++i) {
+                std::smatch match = *i;
+                std::string match_str = match.str();
+                if (match_str.size() >= N) {
+                    result.emplace_back(match_str);
+                }
+            }
+        }
+    }
+    return result;
+}
+
+std::vector<ssu::endpoint>
+client_profile::endpoints() const
+{
+    std::vector<ssu::endpoint> result;
+    {
+        byte_array_iwrap<flurry::iarchive> read(attribute(attribute_tag::endpoints));
+        read.archive() >> result;
+    }
+    return result;
+}
+
+void
+client_profile::set_endpoints(std::vector<ssu::endpoint> const& endpoints)
+{
+    byte_array buf;
+    {
+        byte_array_owrap<flurry::oarchive> write(buf);
+        write.archive() << endpoints;
+    }
+    set_attribute(attribute_tag::endpoints, buf);
+}
+
+} // routing namespace
+} // uia namespace
