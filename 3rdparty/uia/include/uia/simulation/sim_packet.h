ed103fd1942735a325e01b3ac64ea46f600fb2bb
Berkus Decker <berkus@madfire.net>
2017-05-22 02:11:52 +0300

    Update license headers

--- 3rdparty/uia/include/uia/simulation/sim_packet.h
+++ 3rdparty/uia/include/uia/simulation/sim_packet.h
@@ -1,4 +1,13 @@
 //
+// Part of Metta OS. Check https://metta.systems for latest version.
+//
+// Copyright 2007 - 2017, Stanislav Karchebnyy <berkus@metta.systems>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+//
 // Part of Metta OS. Check http://metta.systems for latest version.
 //
 // Copyright 2007 - 2017, Stanislav Karchebnyy <berkus@metta.systems>
