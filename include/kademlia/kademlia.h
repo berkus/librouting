7be97000b16c59a5323f8b0e35523d2748cc3283
Berkus Decker <berkus@madfire.net>
2017-05-22 02:12:14 +0300

    Update license headers

--- include/kademlia/kademlia.h
+++ include/kademlia/kademlia.h
@@ -1,3 +1,12 @@
+//
+// Part of Metta OS. Check https://metta.systems for latest version.
+//
+// Copyright 2007 - 2017, Stanislav Karchebnyy <berkus@metta.systems>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 template <int Bits = 160>
 class kademlia_id
 {
--- lib/regserver_client.cpp
+++ lib/regserver_client.cpp
@@ -1,3 +1,11 @@
+//
+// Part of Metta OS. Check https://metta.systems for latest version.
+//
+// Copyright 2007 - 2017, Stanislav Karchebnyy <berkus@metta.systems>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 #include "routing/private/regserver_client.h"
 #include "uia/comm/socket.h"
 #include "arsenal/file_dump.h"
