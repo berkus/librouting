07e553d03b5e8e76d25a5817139214500b9eba43
Berkus Decker <berkus@madfire.net>
2017-05-22 02:11:22 +0300

    Update license headers

--- 3rdparty/arsenal/lib/settings_provider_linux.cpp
+++ 3rdparty/arsenal/lib/settings_provider_linux.cpp
@@ -1,5 +1,14 @@
+//
+// Part of Metta OS. Check https://metta.systems for latest version.
+//
+// Copyright 2007 - 2017, Stanislav Karchebnyy <berkus@metta.systems>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 // Linux version
 // Save config to ~/.config/<orgname>/appname.config
+//
 #include <string>
 #include <sstream>
 #include <boost/filesystem.hpp>
--- 3rdparty/arsenal/lib/settings_provider_osx.cpp
+++ 3rdparty/arsenal/lib/settings_provider_osx.cpp
@@ -1,5 +1,14 @@
+//
+// Part of Metta OS. Check https://metta.systems for latest version.
+//
+// Copyright 2007 - 2017, Stanislav Karchebnyy <berkus@metta.systems>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 // OSX version
 // Save config to ~/Library/Preferences/<orgdomain>.<appname>.config
+//
 #include <string>
 #include <sstream>
 
